d167e5d534ef3b041545549c943ec50fa81c9c6c
Wesley Michael <wesleymichaelpereira@hotmail.com>
2023-08-31 04:40:16 -0300

    feat:  password encryption before creating credential

--- src/credentials/credentials.repository.ts
+++ src/credentials/credentials.repository.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CredentialDTO } from './dto/credentials.dto';
 import { PrismaService } from '@/prisma/prisma.service';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Cryptr = require('cryptr');
 
 @Injectable()
 export class CredentialsRepository {
+  private code = process.env.SECRET_KEY_CRYPTR;
+  private cryptr = new Cryptr(this.code);
   constructor(private readonly prisma: PrismaService) {}
 
   createCredential(userId: number, credentialsDTO: CredentialDTO) {
     return this.prisma.credentials.create({
       data: {
         ...credentialsDTO,
+        password: this.cryptr.encrypt(credentialsDTO.password),
         userId,
       },
     });
