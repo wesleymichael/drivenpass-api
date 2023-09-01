e2febd2ec2a4d4df923ba7261fe7c5b8a16dc300
Wesley Michael <wesleymichaelpereira@hotmail.com>
2023-09-01 17:34:44 -0300

    feat: config deploy

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log('Server is up and running on port: ' + port);
+  });
 }
 bootstrap();
